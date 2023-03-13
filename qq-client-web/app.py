1b99f3e1a74094bdc107a429c3cba39c382f8be5
intchensc <intchensc@gmail.com>
2023-03-13 12:35:32 +0800

    docs:README更新

--- qq-client-web/app.py
+++ qq-client-web/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import requests
+from flask import Flask, request, url_for, redirect, flash
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    if request.method == "GET":
+        code = request.args.get("code")
+        response2 = requests.get(url='https://api.q.qq.com/sns/jscode2session',
+                                 params={"appid": "1112206385", "secret": "zj1zTHoYqtyhaUHh",
+                                         "js_code": code, "grant_type": "authorization_code"})
+        print(response2.json())
+        return response2.json()
 
 
 if __name__ == '__main__':
---
+++ qq-client/pages/personal/personal.qml
@@ -0,0 +1,6 @@
+<scroll-view class="container">
+  <image bindtap="bindViewTap" src="{{userInfo.avatarUrl}}" class="userinfo-avatar" mode="cover"></image>
+  <text>{{userInfo.nickName}}</text>
+  <button>登录</button>
+</scroll-view>
+
